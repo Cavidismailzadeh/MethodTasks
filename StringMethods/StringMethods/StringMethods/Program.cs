4eade002703e5beb881fc24b136a67a451ada7fa
Cavidismailzadeh <119400508+Cavidismailzadeh@users.noreply.github.com>
2022-12-12 18:46:13 +0400

    String Method

---
+++ StringMethods/StringMethods/StringMethods/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+
+//string address = "Merdekan";
+//Console.WriteLine(address.Length);
+
+
+
+//string name = "Aqshin";
+//Console.WriteLine(name.StartsWith("A"));
+
+
+
+//\
+//string name1 = "Aqshin";
+//Console.WriteLine(name1.StartsWith("a"));
+
+
+
+//string address = "CodeAcademy";
+//Console.WriteLine(address.EndsWith("y"));
+
+
+
+//string address1 = "CodeAcademy";
+//Console.WriteLine(address1.EndsWith("U"));
+
+
+
+
+//string name = "Fazil";
+//Console.WriteLine(name.Contains("a"));
+
+
+
+
+//string name2= "Fazil";
+//Console.WriteLine(name2.Contains("b"));
+
+
+
+
+//string name = "ShaiQ";
+//Console.WriteLine(name.ToLower());
+
+
+
+
+//string name = "shaiq";
+//Console.WriteLine(name.ToUpper());
+
+
+
+
+
+
+
+
